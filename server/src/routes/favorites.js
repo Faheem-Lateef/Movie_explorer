788e70defa02622ad2a7b6f1174c876c90e70125
Faheem-Lateef <faheem.ltst@gmail.com>
2025-08-28 12:44:07 -0700

    Remove all files from Git tracking

--- server/src/routes/favorites.js
+++
@@ -1,7 +0,0 @@
-// server/src/routes/favorites.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/ping', (req, res) => res.json({ msg: 'favorites route OK' }));
-
-module.exports = router;
--- server/src/routes/movies.js
+++
@@ -1,7 +0,0 @@
-// server/src/routes/movies.js
-const express = require('express');
-const router = express.Router();
-
-router.get('/ping', (req, res) => res.json({ msg: 'movies route OK' }));
-
-module.exports = router;
