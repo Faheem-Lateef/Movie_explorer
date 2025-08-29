25b17837532a5b2d7c39483f79b70c411b12be86
Faheem-Lateef <faheem.ltst@gmail.com>
2025-08-29 23:04:59 +0500

    Refactor favorites route to include protected CRUD operations and remove ping endpoint

--- server/src/routes/favorites.js
+++ server/src/routes/favorites.js
@@ -1,7 +1,12 @@
 // server/src/routes/favorites.js
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const favoriteController = require('../controllers/favoriteController');
 
-router.get('/ping', (req, res) => res.json({ msg: 'favorites route OK' }));
+// All routes are protected
+router.post('/', auth, favoriteController.addFavorite);
+router.get('/', auth, favoriteController.getFavorites);
+router.delete('/:id', auth, favoriteController.removeFavorite);
 
 module.exports = router;
