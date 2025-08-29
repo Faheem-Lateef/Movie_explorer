5737d0a5cf2379614212538673912213b2aea59a
Faheem-Lateef <faheem.ltst@gmail.com>
2025-08-29 23:05:09 +0500

    Remove ping endpoint and organize movie search and details routes

--- server/src/routes/movies.js
+++ server/src/routes/movies.js
@@ -1,7 +1,12 @@
 // server/src/routes/movies.js
 const express = require('express');
 const router = express.Router();
+const movieController = require('../controllers/movieController');
 
-router.get('/ping', (req, res) => res.json({ msg: 'movies route OK' }));
+// GET /api/movies/search?q=Batman
+router.get('/search', movieController.search);
+
+// GET /api/movies/:id
+router.get('/:id', movieController.details);
 
 module.exports = router;
